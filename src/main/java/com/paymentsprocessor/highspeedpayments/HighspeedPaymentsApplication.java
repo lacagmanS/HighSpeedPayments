082ede0ebdbcaff4b11178dd9a905931b86eabaf
lacagmanS <salahudinsheikhmuse23@gmail.com>
2025-06-13 00:45:12 +0100

    Initial commit

---
+++ src/main/java/com/paymentsprocessor/highspeedpayments/HighspeedPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.paymentsprocessor.highspeedpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighspeedPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighspeedPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentsprocessor/highspeedpayments/HighspeedPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentsprocessor.highspeedpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighspeedPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
